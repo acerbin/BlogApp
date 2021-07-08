cec6691bf6d84b2397f0facdc10b339bef2f0b0a
Katrina Savova <savova123@gmail.com>
2021-07-08 21:01:50 +0000

    First blood

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require("express"),
+app = express(),
+methodOverride = require("method-override"),
+bodyParser = require("body-parser"),
+mongoose = require("mongoose"),
+expressSanitizer = require("express-sanitizer")
+
+mongoose.connect("mongodb://localhost:27017/blogapp", {useNewUrlParser:true , useUnifiedTopology: true, useFindAndModify: false})
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSanitizer())
+app.use(methodOverride("_method"))
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+// Blog.create({
+// 	title: "Test body",
+// 	image: "https://source.unsplash.com/re2LZOB2XvY",
+// 	body: "Hello from my test body blog lorem ipsum"
+// })
+
+app.get("/blogs",(req, res) => {
+	Blog.find({}, (err, blogs) => {
+		if(err) {
+			console.log(err)
+		} else {
+			res.render("index", {blogs: blogs})			
+		}
+	})
+})
+
+app.get("/",(req, res) => {
+	res.redirect("/blogs")
+})
+
+app.post("/blogs", (req, res) => {
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.create(req.body.blog, (err, newBlog) => {
+		if(err) {
+			res.render("new")
+		} else {
+			res.redirect("/blogs")
+		}
+	})
+})
+
+app.get("/blogs/new", (req, res) => {
+	res.render("new")
+})
+
+app.get("/blogs/:id", (req, res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err) {
+			res.redirect("/blogs")
+		} else {
+			res.render("show", {blog: foundBlog})
+		}
+	})
+})
+
+app.get("/blogs/:id/edit", (req,res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err) {
+			res.redirect("/blogs")
+		} else {
+			res.render("edit", {blog: foundBlog})
+		}
+	})
+} )
+
+app.put("/blogs/:id", (req, res) => {
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+		if(err){
+		   res.redirect("/blogs")
+		 } else {
+		   res.redirect("/blogs/" + req.params.id)
+		 }
+	})
+})
+
+app.delete("/blogs/:id", (req, res) => {
+	Blog.findByIdAndRemove(req.params.id, (err) => {
+		if(err) {
+			res.redirect("/blogs/:id")
+		} else {
+			res.redirect("/blogs")
+		}
+	})
+})
+
+app.listen(3000, function() {
+	console.log("Server listening on port 3000")
+})
